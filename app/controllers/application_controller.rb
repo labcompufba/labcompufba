9f5a7b7c25190d0061b9e87969c51a8e05a616fb
Oscar Resende <oscar@dcc.ufba.br>
2017-01-20 22:36:35 +0000

    TESTE.

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def index
-    render html: "Projeto Laboratórios Compartilhados UFBA!!!"
-  end
-  
-
-end
