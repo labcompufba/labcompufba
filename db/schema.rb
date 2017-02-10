82dca670a182f0d9fc28d1a4d860d319ebe8a3f2
Kenia Guimaraes <keniiarruda@gmail.com>
2017-02-10 12:55:52 +0000

    Adicionando pagina incial da aplicação

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20170209004248) do
 
   create_table "pesquisadors", force: :cascade do |t|
