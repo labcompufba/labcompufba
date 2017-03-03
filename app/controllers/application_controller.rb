1afbc2127d188014c3e7755a069b0ec4f4587888
Kenia Guimaraes <keniiarruda@gmail.com>
2017-03-03 17:09:21 +0000

    Ajustes no layout da pagina inicial e adicionando novas funcionalidades

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
- 
+   # GET /institutos
+  # GET /institutos.json
+ def index
+      @institutos =  Instituto.all
+ end
 end
---
+++ app/views/application/_instituto.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! instituto, :id, :cod_inst, :nome, :endereco, :created_at, :updated_at
+json.url instituto_url(instituto, format: :json)
