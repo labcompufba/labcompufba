d3a0ef80d9736ad3ff78dd2df62f2ca3985710d0
Kenia Guimaraes <keniiarruda@gmail.com>
2017-03-27 18:01:17 +0000

    Pequenos Ajustes

--- db/migrate/20170315112515_create_labequips.rb
+++ db/migrate/20170315112515_create_labequips.rb
@@ -1,11 +1,13 @@
 class CreateLabequips < ActiveRecord::Migration[5.0]
   def change
     create_table :labequips do |t|
-      t.integer :laboratorio_id
-      t.integer :equipamento_id
-      t.integer :quantidade
-      t.string :numero_tombamento
-
+      t.integer  :laboratorio_id
+      t.integer  :equipamento_id
+      t.integer  :quantidade
+      t.string   :numero_tombamento
+      t.boolean  :manutencao
+      t.string   :user_manutencao
+      t.date     :data_manutencao
       t.timestamps
     end
   end
