2c75da94f532ccd9f279ae6c09ab45ebf860328d
Kenia Guimaraes <keniiarruda@gmail.com>
2017-04-02 18:31:09 +0000

    Corrigindo classe de test

--- test/controllers/programas_controller_test.rb
+++ test/controllers/programas_controller_test.rb
@@ -1,48 +1,5 @@
 require 'test_helper'
 
 class ProgramasControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @programa = programas(:one)
-  end
-
-  test "should get index" do
-    get programas_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_programa_url
-    assert_response :success
-  end
-
-  test "should create programa" do
-    assert_difference('Programa.count') do
-      post programas_url, params: { programa: { local: @programa.local, nome: @programa.nome } }
-    end
-
-    assert_redirected_to programa_url(Programa.last)
-  end
-
-  test "should show programa" do
-    get programa_url(@programa)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_programa_url(@programa)
-    assert_response :success
-  end
-
-  test "should update programa" do
-    patch programa_url(@programa), params: { programa: { local: @programa.local, nome: @programa.nome } }
-    assert_redirected_to programa_url(@programa)
-  end
-
-  test "should destroy programa" do
-    assert_difference('Programa.count', -1) do
-      delete programa_url(@programa)
-    end
-
-    assert_redirected_to programas_url
-  end
+ 
 end
--- test/controllers/tipo_servicos_controller_test.rb
+++ test/controllers/tipo_servicos_controller_test.rb
@@ -1,48 +1,5 @@
 require 'test_helper'
 
 class TipoServicosControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @tipo_servico = tipo_servicos(:one)
-  end
-
-  test "should get index" do
-    get tipo_servicos_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_tipo_servico_url
-    assert_response :success
-  end
-
-  test "should create tipo_servico" do
-    assert_difference('TipoServico.count') do
-      post tipo_servicos_url, params: { tipo_servico: { servico: @tipo_servico.servico } }
-    end
-
-    assert_redirected_to tipo_servico_url(TipoServico.last)
-  end
-
-  test "should show tipo_servico" do
-    get tipo_servico_url(@tipo_servico)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_tipo_servico_url(@tipo_servico)
-    assert_response :success
-  end
-
-  test "should update tipo_servico" do
-    patch tipo_servico_url(@tipo_servico), params: { tipo_servico: { servico: @tipo_servico.servico } }
-    assert_redirected_to tipo_servico_url(@tipo_servico)
-  end
-
-  test "should destroy tipo_servico" do
-    assert_difference('TipoServico.count', -1) do
-      delete tipo_servico_url(@tipo_servico)
-    end
-
-    assert_redirected_to tipo_servicos_url
-  end
+  
 end
